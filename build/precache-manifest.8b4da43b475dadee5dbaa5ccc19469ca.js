cdd1bc73877b01c4783cfb22088fe76f79e21091
unknown <cdpuentesf@gmail.com>
2019-11-16 13:48:35 -0500

    Styleguide  1.0 - CesarP

---
+++ build/precache-manifest.8b4da43b475dadee5dbaa5ccc19469ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc05b0a571d89b38187ec57e0df174d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8107222aa48b587a072",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "0d365fc7b5a9ec9f6d6d",
+    "url": "/static/css/main.4bb3dab3.chunk.css"
+  },
+  {
+    "revision": "a8107222aa48b587a072",
+    "url": "/static/js/2.ca6d2386.chunk.js"
+  },
+  {
+    "revision": "0d365fc7b5a9ec9f6d6d",
+    "url": "/static/js/main.93565bd0.chunk.js"
+  },
+  {
+    "revision": "ebb6488b3c0cecef57db",
+    "url": "/static/js/runtime-main.15547109.js"
+  }
+]);
